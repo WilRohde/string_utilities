e44dec88f9a68fa5e42f93303383b7526793b231
WilRohde <warohde@gmail.com>
2022-04-10 12:56:02 -0400

    new string changes???

---
+++ Strings3.js
@@ -0,0 +1,7 @@
+function ParensValid(string) {
+    var arr = string.split("");
+    console.log(arr);
+    return arr;
+}
+const paren1 = '((((d2kfr)kslkdls)kdskkk)';
+console.log(ParensValid(paren1));
---
+++ dist/Strings3.dev.js
@@ -0,0 +1,10 @@
+"use strict";
+
+function ParensValid(string) {
+  var arr = string.split("");
+  console.log(arr);
+  return arr;
+}
+
+var paren1 = '((((d2kfr)kslkdls)kdskkk)';
+console.log(ParensValid(paren1));
